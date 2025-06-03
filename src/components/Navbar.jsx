567af3e2290d8a61176672d31f646c82fff295fc
Rajat Pandey <rajatpndey257@gmail.com>
2025-06-03 10:44:32 +0530

    commit message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -128,6 +128,7 @@ const Navbar = () => {
                 )}
               </div>
             ))}
+            
             {/* Logout Button */}
             <button
               onClick={() => setIsModalOpen(true)}
